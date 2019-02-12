6fa1ee5488366d676f7046302a772c1125da42c4
vinayak hariharmath <vinayak.hariharmath@gmail.com>
2019-02-12 18:48:01 +0530

    example to modules and packages

---
+++ learn/modulesAndPackages/module.py
@@ -0,0 +1,2 @@
+def module():
+    print("module called")
---
+++ learn/modulesAndPackages/sample.py
@@ -0,0 +1,7 @@
+from module import module
+from mainPackage import mainPackage
+from mainPackage.subPackage import subPackage
+
+module()
+mainPackage.main_report()
+subPackage.sub_report()
